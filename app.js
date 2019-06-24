b86befe84db3c5b63c4b3b443c4039caf105e047
Tejas Shailesh Sabunkar <M1048215@mindtree.com>
2019-06-24 22:29:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+xlsxj = require("xlsx-to-json");
+
+xlsxj({
+    input: "sample.xlsx",
+    output: "mapper.json"
+}, function (err, result) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(result);
+    }
+});
+

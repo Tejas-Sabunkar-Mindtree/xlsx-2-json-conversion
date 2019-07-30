b832c5cde1c4340ab908a426d4f3aee77c3f0428
Pallab Nandi <M1043223@mindtree.com>
2019-07-30 12:03:40 +0530

    actions validations

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 xlsxj = require("xlsx-to-json");
+readXlsxFile = require('read-excel-file/node');
 
-xlsxj({
-    input: "sample.xlsx",
-    output: "mapper.json"
-}, function (err, result) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(result);
+const schema = {
+    'actions': {
+        prop: 'actions',
+        parse(value) {
+            return value;
+        }
     }
-});
+}
+
+const regEx = new RegExp('^(?!\s*$)(?:retrieve|subscribe|delete|perform|update|notify| )+$');
+
+readXlsxFile('./sample.xlsx', { schema }).then(({ rows }) => {
+    let rowsNotMatched = 0;
+    rows.forEach(element => {
+        if (!regEx.test(element.actions)) {
+            rowsNotMatched++;
+        }
+    });
+    if (rowsNotMatched === 0) {
+        xlsxj({
+            input: "sample.xlsx",
+            output: "mapper.json"
+        }, function (err, result) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(result);
+            }
+        });
+    } else {
+        console.log('Excel contains invalid actions');
+    }
+})
+
+// xlsxj({
+//     input: "sample.xlsx",
+//     output: "mapper.json"
+// }, function (err, result) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(result);
+//     }
+// });
 
